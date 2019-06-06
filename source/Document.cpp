67183d0b265f534c1f645998fc18574489cda552
zhangmingping <zhangmingping@onlinesign.com.cn>
2019-06-06 13:24:06 +0800

    a little change in Document

--- source/Document.cpp
+++ source/Document.cpp
@@ -1,9 +1,18 @@
 #include "Document.h"
 
 Document::Document()
+	: pdf_(0)
 {
+	pdf = HPDF_New(error_handler, NULL);
+	if (setjmp(env))
+	{
+		HPDF_Free(pdf_);
+		pdf_ = 0;
+	}
 }
 
 Document::~Document()
 {
+	if (pdf_ != 0)
+		HPDF_Free(pdf_);
 }
--- source/Document.h
+++ source/Document.h
@@ -1,11 +1,16 @@
 #ifndef __DOCUMENT__H
 #define __DOCUMENT__H
 
+#include <hpdf.h>
 
 class Document
 {
 public:
 	Document();
 	~Document();
+private:
+	HPDF_Doc pdf_;
+public:
+
 };
 #endif//__DOCUMENT__H
