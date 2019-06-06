7fb25cf1ca658c8d26ff0865ef4ab897ef3cc144
zhangmingping <zhangmingping@onlinesign.com.cn>
2019-06-06 13:56:09 +0800

    added stream and file to document

--- source/Document.cpp
+++ source/Document.cpp
@@ -1,7 +1,11 @@
 #include "Document.h"
 
-Document::Document()
+using std::string;
+using std::vector;
+
+Document::Document(const string& fileName)
 	: pdf_(0)
+	  , fileName(fileName)
 {
 	pdf = HPDF_New(error_handler, NULL);
 	if (setjmp(env))
@@ -15,4 +19,26 @@ Document::~Document()
 {
 	if (pdf_ != 0)
 		HPDF_Free(pdf_);
+}
+
+bool Document::saveToFile()
+{
+	status_ = HPDF_SaveToFile(pdf_, fileName_.c_str());
+	return status_ == HPDF_OK;
+}
+
+vector Document::stream()
+{
+	vector<HPDF_BYTE> vec;
+	status_ = HPDF_SaveToStream(pdf_);
+	if (status_ != HPDF_OK)
+		return vec;
+	auto size = HPDF_GetStreamSize(pdf);
+	if (size == 0)
+		return vec;
+	vec = vector<HPDF_BYTE>(size);
+	status_ = HPDF_ReadFromStream(pdf_, &vec[0], &size);
+	if (status_ != HPDF_OK)
+		return vector<HPDF_BYTE>();
+	return vec;
 }
--- source/Document.h
+++ source/Document.h
@@ -2,15 +2,22 @@
 #define __DOCUMENT__H
 
 #include <hpdf.h>
+#include <string>
+#include <vector>
 
 class Document
 {
 public:
-	Document();
+	Document(const std::string& fileName);
 	~Document();
 private:
 	HPDF_Doc pdf_;
+	std::string fileName_;
+	HPDF_STATUS status_;
 public:
-
+	HPDF_STATUS status()
+	{ return status_;}
+	bool saveToFile();
+	std::vector<HPDF_BYTE> stream();
 };
 #endif//__DOCUMENT__H
